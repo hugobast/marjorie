8bcd39c453600d34519f1d26dba0a3db18cb5f31
Hugo Bastien <hugobast@gmail.com>
2014-05-03 20:14:24 -0400

    Adding dnsmasq

--- spec/acceptance/support/capybara.rb
+++ spec/acceptance/support/capybara.rb
@@ -1,5 +1,5 @@
 require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
-Capybara.app_host = 'http://admin.lvh.me'
+Capybara.app_host = 'http://admin.lebeaujour.ca'
 Capybara.server_port = 3000
