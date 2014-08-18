7bb186971e5e0b586ddf68037d9aa46809d21504
Hugo Bastien <hugobast@gmail.com>
2014-08-17 23:02:01 -0400

    Removes unused jquery plugin

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -3,8 +3,6 @@
 //= require bootstrap
 //= require froala_editor.min
 //= require libs/beautify/beautify-html
-//= require serialize-form
-//= require jquery.serialize-form
 //= require_tree ./admin
 
 try{Typekit.load();}catch(e){}
