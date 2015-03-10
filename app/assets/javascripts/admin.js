c67da74e77d323b6d9b4f501decec924b629096b
Hugo Bastien <hugobast@gmail.com>
2015-03-09 21:45:21 -0400

    Wires up editor to the hidden form field for persistence

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -10,12 +10,15 @@ var $el = $('#content');
 var options = {
   editor: $el[0],
   debug: true,
-  stay: true,
   list: [
-    'insertimage', 'blockquote', 'h2', 'h3', 'p', 'code', 'insertorderedlist', 'insertunorderedlist', 'inserthorizontalrule',
-    'indent', 'outdent', 'bold', 'italic', 'underline', 'createlink'
+    'bold', 'italic', 'underline', 'blockquote', 'insertorderedlist', 'insertunorderedlist',
+    'inserthorizontalrule', 'indent', 'outdent', 'createlink', 'insertimage'
   ]
 };
+
+$('form').on('submit', function() {
+  $('#essay_content').val(pen.getContent());
+});
 
 // create editor
 var pen = window.pen = new Pen(options);
