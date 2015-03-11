3095d7a8383ca6f41a81b6745620f6e0c3288977
Hugo Bastien <hugobast@gmail.com>
2015-03-10 23:40:41 -0400

    Removes upload image from the form, we will make a gallery

--- spec/acceptance/admin_can_upload_images_spec.rb
+++ spec/acceptance/admin_can_upload_images_spec.rb
@@ -11,23 +11,5 @@ feature 'Admin can upload images', %q{
     sign_in! marjorie
   end
 
-  scenario do
-    visit new_admin_essay_path
-
-    attach_image_to_form_and_save
-
-    expect(page).to have_content('Essay was saved successfully')
-  end
-
-  def attach_image_to_form_and_save
-    within('#new_essay') do
-      attach_file 'essay_image', 'spec/fixtures/file.png'
-    end
-
-    save!
-  end
-
-  def save!
-    click_button('Save')
-  end
+  scenario
 end
