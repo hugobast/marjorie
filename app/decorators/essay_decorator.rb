e8c6347dfd5d984edab83fd8459dd41e75abd931
Hugo Bastien <hugobast@gmail.com>
2015-11-12 21:46:52 -0500

    Makes an description about the essay for og

--- app/decorators/essay_decorator.rb
+++ app/decorators/essay_decorator.rb
@@ -1,6 +1,11 @@
 class EssayDecorator < Draper::Decorator
   delegate_all
 
+  def excerpt
+    helpers.truncate parsed_essay_content, \
+      length: 200, separator: ' '
+  end
+
   def share_image
     return default_image unless contains_image?
 
@@ -19,6 +24,12 @@ class EssayDecorator < Draper::Decorator
 
   def parsed_essay
     Nokogiri::HTML(content)
+  end
+
+  def parsed_essay_content
+    parsed_essay.xpath("//text()").to_s
+      .gsub(/\r\n\r\n/, ' ')
+      .gsub(/\r\n/, '')
   end
 
   def parsed_essay_images
--- spec/decorators/essay_decorator_spec.rb
+++ spec/decorators/essay_decorator_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe EssayDecorator do
-  describe '#share_image' do
-    subject { EssayDecorator.new essay }
+  subject { EssayDecorator.new essay }
 
+  describe '#share_image' do
     describe 'when essay contains an image' do
       let(:essay) { create(:essay, :with_image) }
 
@@ -19,6 +19,26 @@ describe EssayDecorator do
       it 'uses the default share image' do
         expect(subject.share_image).to eq \
          "/assets/share-image.png"
+      end
+    end
+  end
+
+  describe '#excerpt' do
+    describe 'when essay content is long' do
+      let(:essay) { create(:essay, :with_image) }
+
+      it 'shows abridged content' do
+        expect(subject.excerpt).to eq \
+          "Well, yeah you&#39;ve gotta lock that down. She calls it a mayonegg. Please refrain from Mayoneggs during this salmonella scare. Heart attack never stopped old big bear. I could use a leather jacket..."
+      end
+    end
+
+    describe 'when essay content is short' do
+      let(:essay) { create(:essay) }
+
+      it 'shows all content' do
+        expect(subject.excerpt).to eq \
+          'Lorem ipsum'
       end
     end
   end
