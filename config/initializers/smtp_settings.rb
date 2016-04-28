5c328280af4e4e3f35e6c3fe0265792cc2782d5a
Hugo Bastien <hugobast@gmail.com>
2016-04-28 07:32:57 -0400

    Swithing to SendGrid

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-  user_name:            ENV['MANDRILL_SMTP_USERNAME'],
-  password:             ENV['MANDRILL_SMTP_PASSWORD'],
+  user_name:            ENV['SENDGRID_SMTP_USERNAME'],
+  password:             ENV['SENDGRID_SMTP_PASSWORD'],
   domain:               'marjorieb.ca',
-  address:              'smtp.mandrillapp.com',
+  address:              'smtp.sendgrid.net',
   port:                 587,
   authentication:       :plain,
   enable_starttls_auto: true
