e8dc032922a748bc72cd16ee96b4160719dc2f94
Hugo Bastien <hugobast@gmail.com>
2017-03-01 20:30:22 -0500

    Separate the og tags
    They are now configurable on each pages. We could probably have defaults
    somewhere though, tbd.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @essays = Essay.front_paged.page params[:page]
-
-    @essays = @essays.archived(year, month) if archive_request?
-  end
-
-  private
-
-  def month
-    params[:month]
-  end
-
-  def year
-    params[:year]
-  end
-
-  def archive_request?
-    params.include?(:year) && params.include?(:month)
   end
 end
